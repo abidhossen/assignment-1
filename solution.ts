000162513ef751613c0603b7394f5d4681a387dc
Md. Abid Hossen <abidhossain3000@gmail.com>
2025-11-18 19:49:42 +0600

    semicolon fixing

--- solution.ts
+++ solution.ts
@@ -1,24 +1,24 @@
 type Format = string | number | boolean;
 function formatValue(input: Format): Format | undefined {
   if (typeof input === 'string') {
-    return input.toUpperCase();
+    return `${input.toUpperCase()};`;
   } else if (typeof input === 'number') {
-    return input * 10;
+    return `${input * 10};`;
   } else if (typeof input === 'boolean') {
     if (input === true) {
-      return false;
+      return `${false};`;
     } else {
-      return true;
+      return `${true};`;
     }
   }
 }
 type LengthFormat = string | (string | Number | Boolean)[];
-function getLength(input: LengthFormat): number | undefined {
+function getLength(input: LengthFormat): number | string | undefined {
   const result = input.length;
   if (typeof input === 'string') {
-    return result;
+    return `${result};`;
   } else if (Array.isArray(input)) {
-    return result;
+    return `${result};`;
   }
 }
 
@@ -33,16 +33,10 @@ class Person {
     return `'Name:${this.name}, Age:${this.age}';`;
   }
 }
-
 interface IbooksFilter {
   title: string;
   rating: number;
 }
-const books: IbooksFilter[] = [
-  { title: 'Book A', rating: 4.5 },
-  { title: 'Book B', rating: 3.2 },
-  { title: 'Book C', rating: 5.0 },
-];
 function filterByRating(arrInput: IbooksFilter[]): IbooksFilter[] {
   return arrInput.filter((element) => element.rating >= 4);
 }
@@ -53,11 +47,6 @@ interface IuserFilter {
   email: string;
   isActive: boolean;
 }
-const users: IuserFilter[] = [
-  { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
-  { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
-  { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
-];
 function filterActiveUsers(users: IuserFilter[]): IuserFilter[] {
   return users.filter((user) => user.isActive === true);
 }
@@ -75,18 +64,8 @@ function printBookDetails(book: Book) {
     }, Available: ${book.isAvailable ? 'Yes' : 'No'}"`
   );
 }
-const myBook: Book = {
-  title: 'Bongoboltu',
-  author: 'Hasina',
-  publishedYear: 2025,
-  isAvailable: true,
-};
-// printBookDetails(myBook);
 
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
 type ArrayFormat = (string | number)[];
-
 function getUniqueValues(arr1: ArrayFormat, arr2: ArrayFormat) {
   const resultArray: ArrayFormat = [];
   const duplicateFinder = (array: ArrayFormat) => {
@@ -102,11 +81,10 @@ function getUniqueValues(arr1: ArrayFormat, arr2: ArrayFormat) {
       }
     }
   };
-  duplicateFinder(array1);
-  duplicateFinder(array2);
+  duplicateFinder(arr1);
+  duplicateFinder(arr2);
   return resultArray;
 }
-// console.log(getUniqueValues(array1, array2));
 
 interface IProducts {
   name: string;
@@ -115,11 +93,6 @@ interface IProducts {
   discount?: number;
 }
 
-const products = [
-  { name: 'Pen', price: 10, quantity: 2 },
-  { name: 'Notebook', price: 25, quantity: 3, discount: 10 },
-  { name: 'Bag', price: 50, quantity: 1, discount: 20 },
-];
 function calculateTotalPrice(arrayInput: IProducts[]) {
   const totalPrice = arrayInput.reduce((totalPrice, product) => {
     if (product.discount) {
@@ -132,4 +105,3 @@ function calculateTotalPrice(arrayInput: IProducts[]) {
 
   return totalPrice;
 }
-console.log(calculateTotalPrice(products));
